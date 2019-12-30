3c55224eafb96694a242b35008475d80d55d6118
Wiktor Kukulski <laxytt@gmail.com>
2019-12-30 11:52:48 +0100

    initial commit

---
+++ App.js
@@ -0,0 +1,14 @@
+import { createAppContainer } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
+import IndexScreen from './src/screens/indexScreen';
+
+const navigator = createStackNavigator({
+  Index: IndexScreen
+}, {
+  initialRouteName: 'Index',
+  defaultNavigationOptions: {
+    title: 'Blogs'
+  }
+});
+
+export default createAppContainer(navigator);
